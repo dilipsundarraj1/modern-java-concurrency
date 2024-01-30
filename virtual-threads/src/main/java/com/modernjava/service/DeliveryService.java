ad64e94d3e3e405a2e85c72ad3e9b3acf49f22a2
Dilip.Sundarraj <dilip.sundarraj@gmail.com>
2024-01-30 09:51:12 -0600

    added the additional logger

--- virtual-threads/src/main/java/com/modernjava/service/DeliveryService.java
+++ virtual-threads/src/main/java/com/modernjava/service/DeliveryService.java
@@ -9,11 +9,14 @@ import com.modernjava.util.LoggerUtil;
 
 import java.util.List;
 
+import static com.modernjava.util.LoggerUtil.log;
+
 public class DeliveryService {
 
     public DeliveryDetails retrieveDeliveryInfo(ProductInfo productInfo){
+        log("retrieving dleivery details for productInfo : " + productInfo);
         CommonUtil.sleep(1000);
-        LoggerUtil.log("retrieveDeliveryInfo after Delay");
+        log("retrieveDeliveryInfo after Delay");
         return new DeliveryDetails(List.of(DeliveryOptionEnum.NEXT_DAY, DeliveryOptionEnum.TWO_DAY));
 
     }
