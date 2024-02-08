0b224ae4097c0f176cee8774259ef543bf3542a5
dilip sundarraj <dilip.bandlasundarraj@gmail.com>
2024-02-08 05:35:27 -0600

    modified the ExploreVirtualThreads

--- virtual-threads/src/main/java/com/modernjava/virtualthreads/ExploreVirtualThreads.java
+++ virtual-threads/src/main/java/com/modernjava/virtualthreads/ExploreVirtualThreads.java
@@ -15,19 +15,6 @@ public class ExploreVirtualThreads {
 
     public static void main(String[] args) {
 
-        var thread1 = Thread.ofVirtual();
-        var thread2 = Thread.ofVirtual();
-//                .unstarted(() -> doSomeWork());
-
-        thread1.start(() -> {
-            // CommonUtil.delay(5000);
-            log("Run task1 in the background");
-
-        });
-        thread2.start(ExploreVirtualThreads::doSomeWork);
-
-        Thread.startVirtualThread(ExploreVirtualThreads::doSomeWork);
-
         log("Program Completed!");
 
         CommonUtil.sleep(2000);
